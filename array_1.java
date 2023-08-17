8d98cbd589b5906a2914dc2f3ff9aa137e600e9e
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-08-17 18:50:56 +0530

    Add files via upload

---
+++ array_1.java
@@ -0,0 +1,17 @@
+package Placement_Course.Array;
+
+public class array_1 {
+	public static void main(String[] args) {
+
+		int[] marks = new int[4];
+		marks[0] = 34;
+		marks[1] = 45;
+		marks[2] = 78;
+		marks[3] = 89;
+
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+		System.out.println(marks[3]);
+	}
+}

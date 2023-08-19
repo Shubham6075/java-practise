c22f6634cab1066614cb9dcbb62931a24fb220fa
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-08-19 21:32:21 +0530

    Add files via upload

---
+++ array_2.java
@@ -0,0 +1,17 @@
+package Placement_Course.Array;
+
+public class array_2 {
+	public static void main(String[] args) {
+
+		// print using for loop
+		int[] marks = new int[4];
+		marks[0] = 34;
+		marks[1] = 45;
+		marks[2] = 78;
+		marks[3] = 89;
+
+		for(int i=0; i< marks.length; i++){
+			System.out.println(marks[i]);
+		}
+	}
+}

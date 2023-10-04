77b8a67f2b0815cf810b21508172f16063f01992
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-10-04 14:43:32 +0530

    Add files via upload

---
+++ Recursion/removeDuplicates_String.java
@@ -0,0 +1,26 @@
+package Placement_Course.Recursion.R1;
+
+public class removeDuplicates_String {
+	public static void main(String[] args){
+
+		String str = "abcadbcefghabi";
+		boolean[] present = new boolean[str.length()];
+		System.out.println(removeDuplicates(str, 0, present));
+
+	}
+
+	public static String removeDuplicates(String str, int idx, boolean[] present){
+		if(idx == str.length()){
+			return "";
+		}
+
+		char curr = str.charAt(idx);
+		if (present[curr-'a']){
+			return removeDuplicates(str,idx+1,present);
+		}else{
+			present[curr-'a'] = true;
+			return curr + removeDuplicates(str,idx+1,present);
+		}
+
+	}
+}

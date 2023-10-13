e1612f98db007776131511404e25c184e824843f
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-10-13 09:29:15 +0530

    Add files via upload

---
+++ Strings/s1.java
@@ -0,0 +1,14 @@
+package Placement_Course.Strings;
+
+public class s1 {
+	public static void main(String[] args){
+
+		// Initializing string
+
+		String name = "Goyat";
+		String name1 = "Lokesh";
+
+		System.out.println(name);
+		System.out.println(name1);
+	}
+}

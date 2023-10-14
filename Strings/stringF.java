d40ca0ed6e2f7a656f4d4589b6977be191d7555c
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-10-14 22:09:57 +0530

    Add files via upload

---
+++ Strings/stringF.java
@@ -0,0 +1,19 @@
+package Placement_Course.Strings;
+
+public class stringF {
+	public static void main(String[] args){
+
+		// ParseInt method
+		String str = "123";
+		int number = Integer.parseInt(str);
+		System.out.println(number);
+
+		System.out.println();
+		System.out.println("^^^^^^^^^^^^^^^^^^^");
+
+		// To string method
+		int num = 139;
+		String str1 = Integer.toString(num);
+		System.out.println(str1.length());
+	}
+}

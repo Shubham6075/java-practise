1ab04872318a87a0bae3cbeaeee80f0163b0469c
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-10-15 13:17:13 +0530

    Add files via upload

---
+++ Strings/s2.java
@@ -0,0 +1,24 @@
+package Placement_Course.Strings;
+
+import java.util.Scanner;
+
+public class s2 {
+
+	// taking input from user in a string
+
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter name :");
+		String s1 = scanner.nextLine();
+
+		System.out.println("Enter name :");
+		String ss = scanner.nextLine();
+
+		System.out.println("^^^^^^^^^^^^^^^^");
+
+		System.out.println(ss);
+		System.out.println(s1);
+	}
+}
